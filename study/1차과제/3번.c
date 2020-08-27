b8f59648e6f2097039fce9b2249890e6b9c00878
wooz3k <70104464+wooz3k@users.noreply.github.com>
2020-08-27 22:51:36 +0900

    3번

---
+++ study/1차과제/3번.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int factorial(int n);
+
+int main()
+{
+	int n;
+	scanf_s("%d", &n);
+	printf("%d", factorial(n));
+  
+  return 0;
+}
+
+int factorial(int n)
+{
+	if (n == 1)
+		return 1;
+
+	return n * factorial(n - 1);
+}
