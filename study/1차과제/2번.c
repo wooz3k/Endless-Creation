6a3f4c8688ce5ac3af4436d19732d7f7e04ad314
wooz3k <70104464+wooz3k@users.noreply.github.com>
2020-08-27 22:44:50 +0900

    2번

---
+++ study/1차과제/2번.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+	int arr[5] = { 3,5,4,2,1 };
+	int tmp;
+
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 4 - i; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+}
